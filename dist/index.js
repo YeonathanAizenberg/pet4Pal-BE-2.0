ca01b8a24db2f7fca76daa6e12eac0cebf8f4b58
YeonathanAizenberg <afula.aizenberg@gmail.com>
2023-04-28 22:49:41 +0300

    untrack git for some files

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const app = express();
-app.get('/', (req, res) => {
-    res.json({ message: "GET METHOD TEST" });
-});
-app.listen("3001", () => {
-    console.log("Server Running??");
-});
-//# sourceMappingURL=index.js.map
